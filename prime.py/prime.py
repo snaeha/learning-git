9b02df2509381fabaad55e718cc13e70aadde458
snaeha <snaeha.shriram@gmail.com>
2022-06-01 11:07:30 -0500

    update of prime

--- prime.py/prime.py
+++ prime.py/prime.py
@@ -5,6 +5,9 @@ import math
 # returns true if number is prime, false if it is not
 
 def isPrime(n):
-    if n % 2 == 0: 
-        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
     return True
+
+print(isPrime(163))
