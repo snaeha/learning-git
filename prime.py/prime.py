c6912e6ac9e6a458c6ada1e23796a21a862289d7
sanjana <sanjanashriram2012@gmail.com>
2022-06-01 11:29:34 -0500

    negatives

--- prime.py/prime.py
+++ prime.py/prime.py
@@ -5,7 +5,11 @@ import math
 # returns true if number is prime, false if it is not
 
 def isPrime(n):
-    for i in range(2,n):
+    if n < 2:
+        return False
+    if n % 2 == 0:
+        return False
+    for i in range(3, n, 2):
         if n % i == 0:
             return False
     return True
